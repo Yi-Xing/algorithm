2e666d55ec774eb5079c844cbb2f87dc3f40bb91
Yi-Xing <1160589090@qq.com>
2020-09-17 19:23:53 +0800

    9.16打卡

--- src/力扣/普通题/简单/SerialNumber226/Solution.java
+++ src/力扣/普通题/简单/SerialNumber226/Solution.java
@@ -1,7 +1,29 @@
 package 力扣.普通题.简单.SerialNumber226;
 
+
 /**
  * @date 2020-09-16
  */
 public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }
