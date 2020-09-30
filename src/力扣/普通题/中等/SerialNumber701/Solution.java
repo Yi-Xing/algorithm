a05ac83d21ba33a7b0bc66e16c6f4ec8a1b69991
Yi-Xing <1160589090@qq.com>
2020-09-30 22:24:05 +0800

    9.30打卡

---
+++ src/力扣/普通题/中等/SerialNumber701/Solution.java
@@ -0,0 +1,38 @@
+package 力扣.普通题.中等.SerialNumber701;
+
+/**
+ * @date 2020-09-14
+ */
+public class Solution {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
